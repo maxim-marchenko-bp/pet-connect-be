8c9763a6b8fd10af95edd077abb66b7c533a070b
Maxim Marchenko <maxim-marchenko-bp@gmail.com>
2026-01-09 16:17:20 +0200

    refactor: migrate to TypeScript and restructure application files

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { AppDataSource } from "./data-source";
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+AppDataSource.initialize()
+  .then(() => console.log('Data source initialized'))
+  .catch((error: Error) => console.error('Error during data source initialization:', error));
+
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
